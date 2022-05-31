3abef6dea25c3213e43e34c1f3f8382cbd1565e3
Krzysiek Jankowski <pikaknight@outlook.com>
2022-05-31 19:56:08 +0200

    added comments (temp)

--- RecipeBook48/FormWelcome.cs
+++ RecipeBook48/FormWelcome.cs
@@ -22,18 +22,18 @@ namespace RecipeBook48
 
         private void Form1_Load(object sender, EventArgs e)
         {
-                
+              //todo
 
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-
+            //todo
         }
 
         private void pictureBox1_Click_1(object sender, EventArgs e)
         {
-
+            //todo
         }
 
 
@@ -64,7 +64,7 @@ namespace RecipeBook48
 
         private void FormWelcome_FormClosing(object sender, FormClosingEventArgs e)
         {
-        
+            //todo later
         }
 
         private void TileAddRecipe_Click(object sender, EventArgs e)
